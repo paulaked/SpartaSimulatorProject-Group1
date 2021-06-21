6cd5c5657019c0b0b02bc0838db2114179f7ff6a
MattBrackenbury <matt.a.brackenbury@gmail.com>
2021-06-21 10:50:35 +0100

    trainee generator creation

--- trainee_generator.py
+++ trainee_generator.py
@@ -1,1 +1,6 @@
-import random
+from random import randrange
+
+
+def rand_trainee():  # calling function returns a new random number. Need to assign to variable each month so maths
+    # and print statements match
+    return randrange(20, 31, 1)  # generates number between 20-30, increment 1.
