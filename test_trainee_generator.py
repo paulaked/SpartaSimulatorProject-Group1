8a2c78a5a32a6e715429ef9e224738824ee8963c
Sam Duke <84142865+SamCDuke@users.noreply.github.com>
2021-06-23 09:46:08 +0100

    Final day update of all files.

--- test_trainee_generator.py
+++ test_trainee_generator.py
@@ -1,1 +1,28 @@
-import trainee_generator
+import unittest
+from trainee_generator import rand_new_trainees, waiting_list
+
+
+#  Testing the random number generator for monthly trainee intake.
+class TestRandGen(unittest.TestCase):
+
+    #  Tests whether an integer is created first.
+    def test_num_is_int(self):
+        self.assertIsInstance(rand_new_trainees(), int)
+
+    #  Tests whether the integer falls between 20 and 30.
+    def test_num_in_range(self):
+        self.assertGreaterEqual(rand_new_trainees(), 20)
+        self.assertLessEqual(rand_new_trainees(), 30)
+
+
+#  Testing that the waiting_list output is actually a list
+class ListTests(unittest.TestCase):
+    def test_trainees(self):
+        self.assertIsInstance(waiting_list, list)
+
+
+#  Currently unrequired test
+# class TestNewTraineeAdd(unittest.TestCase):
+#
+#     def test_trainee_added(self):
+
--- trainee_generator.py
+++ trainee_generator.py
@@ -7,13 +7,17 @@ def rand_new_trainees():
     return randrange(20, 31, 1)  # generates number between 20-30, increment 1.
 
 
+#  Creating a global variable for the student ID counter as this will need to retain the count outside of the function
+#  it is used in.
 stud_id_counter = 0
 
 
+#  A function that adds new trainees to the waiting list based on a randomly generated number in a range.
 def new_trainee_add():
     global waiting_list
     global stud_id_counter
-    trainees = rand_new_trainees()  # assigns random number of trainees to be created
-    for i in range(1, trainees + 1):  # adds each trainee to the waiting list
-        waiting_list.append(stud_id_counter + i)  # assigns trainees ID numbers
-    stud_id_counter += trainees  # adds number of trainees to ID so no duplicate ID values
+    trainees = rand_new_trainees()  # Assigns random number of trainees to be created
+    for i in range(1, trainees + 1):  # Adds each trainee to the waiting list
+        waiting_list.append(stud_id_counter + i)  # Assigns trainees ID numbers
+    stud_id_counter += trainees  # Adds number of trainees to ID so no duplicate ID values
+    return stud_id_counter
